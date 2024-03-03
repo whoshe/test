a7e44ae6d6d5c8d016813c43242ff94c659f8de4
Changwoo Nam <whoshe@funnyplan.com>
2024-03-03 10:51:49 +0900

    중첩 레이아웃 버튼 링크 버그 해결

--- src/component/header.js
+++ src/component/header.js
@@ -3,9 +3,12 @@ import  Link from 'next/link';
 export default function Header () {
     return (
     <>
-    <h1>메인 머리글</h1> 
-    <Link href='/'>홈</Link>
-    
+    <nav className='flex gap-8 justify-center items-center'>
     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button">Open drawer</label>
+    <Link href='/'>홈</Link>
+    <h1>메인 머리글</h1> 
+    </nav>
+    
+    
     </>)
 }
