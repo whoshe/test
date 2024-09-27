293f7cbb15e15dbd730ac9d68def828a5ed15ae4
Changwoo Nam <whoshe@funnyplan.com>
2024-09-28 01:28:33 +0900

    날짜 한글로 변환

--- src/component/date.js
+++ src/component/date.js
@@ -1,6 +1,15 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format, formatDistanceToNow } from 'date-fns'
+import { ko } from 'date-fns/locale'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  return (
+    <time dateTime={dateString}>
+      {format(date, 'PPP EEE', { locale: ko })}
+      {/* {formatDistance(new Date(date), new Date(), {
+        addSuffix: true,
+        locale: ko,
+      })} */}
+    </time>
+  )
 }
