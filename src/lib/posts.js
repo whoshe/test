dbd8a5cf36be215467b15de3a381e6c250244027
Changwoo Nam <whoshe@funnyplan.com>
2024-09-27 19:20:48 +0900

    recent posts 추가

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -5,7 +5,6 @@ import { remark } from 'remark'
 import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), '/src/posts')
-
 export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
